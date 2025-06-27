9c20e0ecc9f201dbc9ed73049efecff434ef8100
Vicodertoten <victorryelandt2@gmail.com>
2025-06-27 15:15:10 +0200

    final pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'assets/*.png'],
+      manifest: {
+        name: 'Inaturamouche',
+        short_name: 'Inaturamouche',
+        description: 'Un quiz pour tester vos connaissances sur la nature.',
+        theme_color: '#283618',
+        background_color: '#283618',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
