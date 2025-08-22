c41fb816f0a21ad0a8b3c6a03fb9d856448a3171
Vicodertoten <victorryelandt2@gmail.com>
2025-08-22 22:39:48 +0200

    feat: cache critical assets

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'leaflet/dist/leaflet.css';
 import App from './App.jsx'
+import { registerSW } from 'virtual:pwa-register'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+registerSW({ immediate: true })
