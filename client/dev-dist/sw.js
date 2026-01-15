46dc7890d5f2787401858099e79b4a21f445e20e
“Vicodertoten” <victorryelandt2@gmail.com>
2026-01-15 23:58:24 +0100

    zae

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-bfd2dca0'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.hnjsifv1ofg"
+    "revision": "0.gpgqq5f51i8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
